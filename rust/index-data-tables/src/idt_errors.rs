1fb45a0eef65ede14f4b4a1b1628aacbbf18f488
Jacob Wiens <jacobdwiens@gmail.com>
2026-05-04 13:27:06 -0700

    Refactored IDT Errors Module and added IDTReadError to handle cases where the parser encounters errors from I/O or other sources

--- rust/index-data-tables/src/idt_errors.rs
+++ rust/index-data-tables/src/idt_errors.rs
@@ -1,88 +1,39 @@
 use std::{error::Error, fmt::Display};
 
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum IDTErrorType {
-    IDTFormatError,
-    IDTValidationError,
-    IDTMetadataError,
-    IDTParsingError,
-}
-
 #[derive(Debug)]
-pub struct IDTError {
-    variant: IDTErrorType,
-    message: String,
-    source: Option<Box<dyn Error + 'static>>
+pub enum IDTError {
+    IDTFormatError(String),
+    IDTValidationError(String),
+    IDTMetadataError(String),
+    IDTParsingError(String),
+    ReadError(Box<dyn Error + 'static>)
 }
 
 impl Display for IDTError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.variant {
-            IDTErrorType::IDTFormatError =>
-                write!(f, "IDTFormatError: {}", self.message),
-            IDTErrorType::IDTValidationError =>
-                write!(f, "IDTFormatError: {}", self.message),
-            IDTErrorType::IDTMetadataError =>
-                write!(f, "IDTFormatError: {}", self.message),
-            IDTErrorType::IDTParsingError =>
-                write!(f, "IDTFormatError: {}", self.message),
+        match self {
+            IDTError::IDTFormatError(message) =>
+                write!(f, "IDT FORMAT ERROR: {}", message),
+            IDTError::IDTValidationError(message) =>
+                write!(f, "IDT VALIDATION ERROR: {}", message),
+            IDTError::IDTMetadataError(message) =>
+                write!(f, "IDT METADATA ERROR: {}", message),
+            IDTError::IDTParsingError(message) =>
+                write!(f, "IDT PARSING ERROR: {}", message),
+            IDTError::ReadError(source) =>
+                write!(f, "IDT Parser File Read failed with:\n{}", source),
         }
     }
 }
 
 impl Error for IDTError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
-        self.source.as_deref()
-    }
-}
-
-impl IDTError {
-    /**
-     * Returns an IDT Format Error with the given message
-     * IDT Format Errors do not have source errors
-    */
-    pub fn idt_format_error(message: String) -> IDTError {
-        IDTError { 
-            variant: IDTErrorType::IDTFormatError,
-            message: message,
-            source: None,
-        }
-    }
-
-    /**
-     * Returns an IDT Validation Error with the given message
-     * IDT Format Errors do not have source errors
-    */
-    pub fn idt_validation_error(message: String) -> IDTError {
-        IDTError { 
-            variant: IDTErrorType::IDTValidationError,
-            message: message,
-            source: None,
-        }
-    }
-
-    /**
-     * Returns an IDT Metadata Error with the given message
-     * IDT Format Errors do not have source errors
-    */
-    pub fn idt_metadata_error(message: String) -> IDTError {
-        IDTError { 
-            variant: IDTErrorType::IDTMetadataError,
-            message: message,
-            source: None,
-        }
-    }
-
-    /**
-     * Returns an IDT Validation Error with the given message
-     * IDT Format Errors do not have source errors
-    */
-    pub fn idt_parsing_error(message: String) -> IDTError {
-        IDTError { 
-            variant: IDTErrorType::IDTParsingError,
-            message: message,
-            source: None,
+        match self {
+            IDTError::ReadError(source) =>
+                Some(source.as_ref()),
+            _ => 
+                None,
         }
     }
 }
